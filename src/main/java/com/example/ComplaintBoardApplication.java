cb1319fe81b4df95fc79f04ecd8ec0d63d83a323
ozaki25 <fs65ugumori@yahoo.co.jp>
2017-12-29 11:03:03 +0900

    init

---
+++ src/main/java/com/example/ComplaintBoardApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintBoardApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ComplaintBoardApplication.class, args);
+  }
+}
